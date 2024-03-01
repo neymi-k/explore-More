1bdd40c2370c1001570a57af4782d7436ee7b2a7
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-03-01 11:30:54 +0100

    sign up tested and working and comments added

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,25 @@
 //importa las interfaces Request y Response de Express para ser utilizadas en el código.
 import { Request, Response } from "express";
+import { prismaClient } from "../schema";
+import { hashSync } from 'bcrypt';
 
-//La función login toma dos parámetros, req y res, que representan la solicitud y la respuesta respectivamente.
-export const login = (req:Request, res:Response) => {
-    res.send('login works'); //avisa que el login funciono
+//La función sign up, para crear usuario toma dos parámetros, req y res, que representan la solicitud y la respuesta respectivamente.
+export const signup = async (req:Request, res:Response) => {
+    const {email, password, name} = req.body;
+
+    //se verifica si el correo ya esta en la base de datos
+    let user = await prismaClient.user.findFirst({ where: {email}})
+    if (user) {
+        throw Error('User already exists!') //si esta el correo se le bota de la autenticacion
+    }
+
+    //se espera del usuario nombre, correo y mail
+    user = await prismaClient.user.create({
+        data:{
+            name,
+            email,
+            password: hashSync(password, 10)
+        }
+    })
+    res.json(user)
 }
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,11 +1,19 @@
 import express, {Express, Request, Response} from 'express';
 import { PORT } from './secrets'
 import rootRouter from '../routes';
+import { PrismaClient } from '@prisma/client'; //configuración del cliente Prisma
 
 const app:Express = express(); 
+
+app.use(express.json())
 
 //ahora usemos el router del login
 app.use('/api', rootRouter);
 
-//pruebo si esta operativo
+//registrar en la consola eventos relacionados con consultas específicamente
+export const prismaClient = new PrismaClient({
+    log:['query']
+})
+
+//pruebo si esta operativo el puerto
 app.listen(PORT, () => {console.log('App working')})
