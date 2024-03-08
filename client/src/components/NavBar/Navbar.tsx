cd599df1b2f71849429f3937892a45d36943278a
Yeshiprinss <martinezyessenia068@gmail.com>
2024-03-08 15:52:50 +0100

    addess window modal

--- client/src/components/NavBar/Navbar.tsx
+++ client/src/components/NavBar/Navbar.tsx
@@ -7,6 +7,8 @@ import { FaCircleUser } from "react-icons/fa6";
 
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isProfileModalOpen, setIsProfileModalOpen] = useState(false);
+
 
   return (
     <nav className="relative flex items-center justify-between p-5 bg-amber-500 gap-20">
@@ -24,14 +26,23 @@ function NavBar() {
         <div className="hidden lg:flex gap-10 p-5 mr-20">
           <a href="#Home" className="text-orange px-2 flex-grow hover:text-white transform hover:scale-110 transition-transform duration-200">Home</a>
           <a href="#Form" className="text-orange px-2 flex-grow hover:text-white transform hover:scale-110 transition-transform duration-200">Contac</a>
-          <a href="/login" className="text-orange px-2 flex-grow hover:text-white transform hover:scale-110 transition-transform duration-200">Sing Out</a>
           <a href="/" className="text-orange px-2 flex-grow hover:text-white transform hover:scale-110 transition-transform duration-200">Add Product</a>
         </div>
         <img src={logo} alt="Logo" className="w-24 h-50" />
       </div>
 
-      <BsBackpackFill className="text-8xl sm:text-8xl md:text-7xl lg:text-5xl xl:text-5xl text-white hover:text-black" />
-      <FaCircleUser className="text-8xl sm:text-8xl md:text-7xl lg:text-5xl xl:text-5xl text-white hover:text-black" />
+      <BsBackpackFill className="text-9xl sm:text-8xl md:text-7xl lg:text-5xl xl:text-5xl text-white hover:text-black"/>
+      <FaCircleUser className="text-9xl sm:text-8xl md:text-7xl lg:text-5xl xl:text-5xl text-white hover:text-black" onClick={() => setIsProfileModalOpen(true)}/>
+
+      <div className={`fixed inset-0 bg-black bg-opacity-50 ${isProfileModalOpen ? 'block' : 'hidden'}`} onClick={() => setIsProfileModalOpen(false)}>
+ <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-amber p-4 rounded text-center">
+    <a href="/" className="block text-white px-2 hover:text-amber-500 transform hover:scale-110 transition-transform duration-200">Your reservations</a>
+    <a href="/" className="block text-white px-2 hover:text-amber-500 transform hover:scale-110 transition-transform duration-200">Your products</a>
+    <a href="/" className="block text-white px-2 hover:text-amber-500 transform hover:scale-110 transition-transform duration-200">Sign out</a>
+ </div>
+</div>
+
+
 
 
 
@@ -39,7 +50,6 @@ function NavBar() {
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-amber p-4 rounded text-center">
           <a href="#Home" className="block text-white px-2 hover:text-amber-500 transform hover:scale-110 transition-transform duration-200">Home</a>
           <a href="#Form" className="block text-white px-2 hover:text-amber-500 transform hover:scale-110 transition-transform duration-200">Contac</a>
-          <a href="/login" className="block text-white px-2 hover:text-amber-500 transform hover:scale-110 transition-transform duration-200">Sing Out</a>
           <a href="/" className="block text-white px-2 hover:text-amber-500 transform hover:scale-110 transition-transform duration-200">Add Product</a>
         </div>
 
