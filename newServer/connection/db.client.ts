8c8cd685ff54aa047842e527e5040a2902d7eddc
Paolo Damen <paolodamen@gmail.com>
2024-03-14 13:46:50 +0100

    newServer new folder structure

--- newServer/connection/db.client.ts
+++ newServer/connection/db.client.ts
@@ -2,7 +2,16 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    const users = await prisma.user.findMany()
+    const users = await prisma.user.create({
+      data:  {
+        name: "Betty",
+        email: "betty@yahoo.com",
+        password: "abc123",
+        role: "USER"
+      }
+    })
+
+
     console.log(users)
 }
 
---
+++ newServer/main.ts
@@ -0,0 +1,8 @@
+import { Server } from "./models/server.model";
+
+function main() {
+    const server = new Server();
+    server.listen();
+}
+
+main();
