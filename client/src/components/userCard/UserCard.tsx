a9c1cba9d1bdd154f443b723046fa51bba3071c7
Yeshiprinss <martinezyessenia068@gmail.com>
2024-03-14 18:45:12 +0100

    change in style usercard

--- client/src/components/userCard/UserCard.tsx
+++ client/src/components/userCard/UserCard.tsx
@@ -7,7 +7,7 @@ const UserCard = ({ user }) => {
 
     return (
         <div className="flex justify-center items-center">
-            <div className="w-[500px] h-[120px] p-[20px] bg-gray-400  flex flex-row justify-between m-5">
+            <div className="w-[500px] h-[120px] p-[20px] bg-gray-400  flex flex-row justify-between m-5 border-5px">
                 <div className="flex flex-col">
                 <img src={user.avatar} alt="Avatar del usuario" className="w-10 h-10 rounded-full mr-2 sm:mr-4" />
                     <h1 className='font-bold'>{user.name}</h1>
@@ -16,7 +16,7 @@ const UserCard = ({ user }) => {
 
 
 
-                <button className="bg-amber-500 hover:bg-black text-white font-bold px-1 rounded m-5 sm:m-5">
+                <button className="bg-amber-500 hover:bg-black text-white font-bold px-1  m-5 ">
                     Eliminar
                 </button>
             </div>
