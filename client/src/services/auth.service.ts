de7939a4b13bae53c6e92d76047a50833a6e884d
Paolo Damen <paolodamen@gmail.com>
2024-03-14 15:29:55 +0100

    changes

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -1,15 +1,20 @@
 import axios from "axios";
 
-const API_URL = "https://jsonplaceholder.typicode.com/users";
-
+// const API_URL = "https://jsonplaceholder.typicode.com/users";
+const API_URL = "http://localhost:3000/api/auth/"
 export const fetchUsers = async () => {
   const response = await axios.get(API_URL);
   return response.data;
 };
 
-export const register = (username: string, email: string, password: string) => {
-  return axios.post(API_URL + "signup", { username, email, password });
+export const register = (name: string, email: string, password: string) => {
+  return axios.post(API_URL + "signup", { name, email, password });
 };
+
+
+
+
+
 
 export const login = async (email: string, password: string) => {
   return axios.post(API_URL + "login", { email, password }).then((response) => {
