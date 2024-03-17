0b68649ea5c32c15decc25cea344824e704a5369
Paolo Damen <paolodamen@gmail.com>
2024-03-17 17:26:12 +0100

    changes names controller model routes

--- new-server/routes/trip.routes.ts
+++ new-server/routes/trip.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import TripController from "../controllers/TripController";
+import TripController from "../controllers/trip.controller";
 
 const TripRouter = Router();
 
